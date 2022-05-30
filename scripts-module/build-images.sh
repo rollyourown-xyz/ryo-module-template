586f5436c5988800abdaf5893ee36f950854de34
xyzroller <xyzroller@rollyourown.xyz>
2022-05-30 20:24:22 +0200

    Make remote build optional

--- scripts-module/build-images.sh
+++ scripts-module/build-images.sh
@@ -20,6 +20,7 @@ helpMessage()
    echo "Flags:"
    echo -e "-n hostname \t\t\t(Mandatory) Name of the host for which to build images"
    echo -e "-v version \t\t\t(Mandatory) Version stamp to apply to images, e.g. 20210101-1"
+   echo -e "-r remote_build \t\t(Mandatory) Whether to build images on the remote LXD host (true/false)"
    echo -e "-m mariadb_version \t(Optional) Override default mariadb version to use for the mariadb image, e.g. 10.5.13 (default)"
    echo -e "-h \t\t\t\tPrint this help message"
    echo ""
@@ -35,18 +36,19 @@ errorMessage()
 
 # Add additional options for software package versions defined above
 # Option "-m mariadb_version" as an example
-while getopts n:v:m:h flag
+while getopts n:v:r:m:h flag
 do
     case "${flag}" in
         n) hostname=${OPTARG};;
         v) version=${OPTARG};;
+        r) remote_build=${OPTARG};;
         m) mariadb_version=${OPTARG};;
         h) helpMessage ;;
         ?) errorMessage ;;
     esac
 done
 
-if [ -z "$hostname" ] || [ -z "$version" ] || [ -z "$mariadb_version" ]
+if [ -z "$hostname" ] || [ -z "$version" ] || [ -z "$remote_build" ] || [ -z "$mariadb_version" ]
 then
    errorMessage
 fi
@@ -58,7 +60,7 @@ MODULE_ID="$(yq eval '.module_id' "$SCRIPT_DIR"/../configuration/configuration.y
 # Module-specific image builds here..., for example
 # echo "Building images for "$MODULE_ID" module on "$hostname""
 # echo "Building COMPONENT image"
-# echo "Executing command: packer build -var \"host_id="$hostname"\" -var \"version="$version"\" -var \"mariadb_version="$mariadb_version"\" "$SCRIPT_DIR"/../image-build/TEMPLATE.pkr.hcl"
+# echo "Executing command: packer build -var \"host_id="$hostname"\" -var \"version="$version"\" -var \"remote="$remote_build"\" -var \"mariadb_version="$mariadb_version"\" "$SCRIPT_DIR"/../image-build/TEMPLATE.pkr.hcl"
 # echo ""
-# packer build -var "host_id="$hostname"" -var "version="$version"" -var "mariadb_version="$mariadb_version"" "$SCRIPT_DIR"/../image-build/TEMPLATE.pkr.hcl
+# packer build -var "host_id="$hostname"" -var "version="$version"" -var "remote="$remote_build"" -var "mariadb_version="$mariadb_version"" "$SCRIPT_DIR"/../image-build/TEMPLATE.pkr.hcl
 # echo ""
